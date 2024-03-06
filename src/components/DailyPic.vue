e33cafafa71e647212daaa52162af494758b4d90
Ryan <ryanjerickson@gmail.com>
2024-03-06 14:30:01 -0600

    fetch

--- src/components/DailyPic.vue
+++ src/components/DailyPic.vue
@@ -1,6 +1,38 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
+interface PicData {
+    title: string,
+    date: string,
+    hdurl: string,
+    explanation: string
+}
+
+const picData = ref<PicData>({title: '', date: '', hdurl: '', explanation: ''})
+
+async function getData() {
+    const res = await fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    const data = await res.json()
+    picData.value = data
+}
+
+getData()
+
 </script>
 
 <template>
     <h1>Nasa Pic of the Day</h1>
+    <div class="daily-pic-container">
+    {{ picData.date }}
+    <img :src="picData.hdurl" />
+    {{ picData.explanation }}
+    </div>
 </template>
+
+<style scoped>
+.daily-pic-container {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+}
+</style>
