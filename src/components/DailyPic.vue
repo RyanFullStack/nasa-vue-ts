a1eed4efdf941675bca73309ebf17dc920bdc49d
Ryan <ryanjerickson@gmail.com>
2024-03-08 09:53:40 -0600

    date buttons

--- src/components/DailyPic.vue
+++ src/components/DailyPic.vue
@@ -1,50 +1,60 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 
 interface PicData {
-    title: string,
-    date: string,
-    url: string,
-    explanation: string
+  title: string;
+  date: string;
+  url: string;
+  explanation: string;
 }
 
-const picData = ref<PicData>({title: '', date: '', url: '', explanation: ''})
+const picData = ref<PicData>({ title: "", date: "", url: "", explanation: "" });
 
 async function getData() {
-    const res = await fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-    const data = await res.json()
-    picData.value = data
+  const res = await fetch(
+    "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
+  );
+  const data = await res.json();
+  picData.value = data;
 }
 
-getData()
-
+getData();
 </script>
 
 <template>
+  <div class="daily-pic-container">
     <h1>Pic Of The Day</h1>
-    <div class="daily-pic-container">
+    <div class="date-buttons">
+      <button>-1 Day</button>
+      <button>Today</button>
+      <button>+1 Day</button>
+    </div>
     <h2>{{ picData.title }}</h2>
     {{ picData.date }}
     <img :src="picData.url" />
     <p>{{ picData.explanation }}</p>
-    </div>
+  </div>
 </template>
 
 <style scoped>
 .daily-pic-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  max-width: 1600px;
+  width: 100%;
+}
+.date-buttons {
     display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    gap: 8px;
-    max-width: 1600px;
-    width: 100%;
+    gap: 5px;
 }
 img {
-    max-width: 1600px;
-    width: 90%;
+  max-width: 1600px;
+  width: 90%;
 }
 p {
-    max-width: 1400px;
+  max-width: 1400px;
 }
 </style>
