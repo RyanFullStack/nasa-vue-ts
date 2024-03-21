8ef3004052954138a8377e491773706bf787caea
Ryan <ryanjerickson@gmail.com>
2024-03-21 11:37:25 -0500

    mars style

--- src/components/MarsPics.vue
+++ src/components/MarsPics.vue
@@ -5,3 +5,20 @@
     <h1>Mars Pics</h1>
   </div>
 </template>
+
+<style scoped>
+.mars-pics-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  max-width: 1600px;
+  width: 100%;
+}
+h1 {
+  font-size: 3.2em;
+  line-height: 1.1;
+  margin: 10px auto 10px;
+}
+</style>
