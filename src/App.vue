44f687a0eac488ab0a063c20b9e40bb610473de0
Ryan <ryanjerickson@gmail.com>
2024-03-21 11:25:15 -0500

    select

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
+import ref from 'vue'
 import DailyPic from './components/DailyPic.vue'
+
+const selectedComponent = ref<string>('dailypic')
+
+const components = {
+  dailypic: DailyPic
+}
 </script>
 
 <template>
-  <DailyPic/>
+  <select v-model="selectedComponent">
+    <option value="dailypic">Daily Pic</option>
+    <option value="marspics">Mars Pics</option>
+  </select>
+  <component :is="components[SelectedComponent]" />
 </template>
